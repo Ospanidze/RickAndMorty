a9270f2e1a3aa98f64294dc74678c678972a5d56
Aida <aidar.ospan@list.ru>
2023-03-24 20:40:21 +0600

    update 0.0.1

---
+++ 3.02/ViewControllers/TestViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TestViewController.swift
+//  3.02
+//
+//  Created by Айдар Оспанов on 24.03.2023.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+    
+    @IBOutlet var testCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
---
+++ 3.02/Views/testActionCell.swift
@@ -0,0 +1,14 @@
+//
+//  testActionCell.swift
+//  3.02
+//
+//  Created by Айдар Оспанов on 24.03.2023.
+//
+
+import UIKit
+
+class testActionCell: UICollectionViewCell {
+    
+    @IBOutlet var testLabel: UILabel!
+}
+ 
